0862237e4057b0fca664af347523b9f73a94be12
sahalnazar <sahalnazar007@gmail.com>
2020-10-03 11:13:56 +0530

    Implement Moshi

--- Restful-Restrofit/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ Restful-Restrofit/app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -22,6 +22,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.android.marsrealestate.network.MarsApi
+import com.example.android.marsrealestate.network.MarsProperty
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -51,12 +52,12 @@ class OverviewViewModel : ViewModel() {
     private fun getMarsRealEstateProperties() {
 //        _response.value = "Set the Mars API Response here!"
 
-        MarsApi.retrofitService.getProperties().enqueue(object: Callback<String>{
-            override fun onResponse(call: Call<String>, response: Response<String>) {
-                _response.value = response.body()
+        MarsApi.retrofitService.getProperties().enqueue(object: Callback<List<MarsProperty>>{
+            override fun onResponse(call: Call<List<MarsProperty>>, response: Response<List<MarsProperty>>) {
+                _response.value = "Success: ${response.body()?.size} Mars properties!"
             }
 
-            override fun onFailure(call: Call<String>, t: Throwable) {
+            override fun onFailure(call: Call<List<MarsProperty>>, t: Throwable) {
                 _response.value = t.message
             }
 
